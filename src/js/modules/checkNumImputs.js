2da6f88bcf7630182d26b7f1e8eabda162b4d924
Alexander <kondrasasha@mail.ru>
2022-03-11 00:58:41 +0300

    First commit

---
+++ src/js/modules/checkNumImputs.js
@@ -0,0 +1,11 @@
+const checkNumImputs = (selector) => {
+    const numImputs = document.querySelectorAll(selector);
+
+    numImputs.forEach(item => {
+        item.addEventListener('input', () => {
+            item.value = item.value.replace(/\D/, '');
+        });
+    });
+};
+
+export default checkNumImputs;
